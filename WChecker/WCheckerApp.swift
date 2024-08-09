c47155e8d1c69b92bf82d4f1b8b5b75e42c8c7ba
jubayer <jubayertalha194@gmail.com>
2024-08-09 22:11:08 +0600

    Initial Commit

---
+++ WChecker/WCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WCheckerApp.swift
+//  WChecker
+//
+//  Created by Talha Jubayer on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WCheckerUITests/WCheckerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WCheckerUITestsLaunchTests.swift
+//  WCheckerUITests
+//
+//  Created by Talha Jubayer on 9/8/24.
+//
+
+import XCTest
+
+final class WCheckerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
